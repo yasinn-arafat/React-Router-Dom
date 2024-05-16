2d0e35d3d62ac87b8d512b83243b99c6104e7c93
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-05-16 23:02:19 +0600

    Project Created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./Component/RootLayout";
+
+function App() {
+  return <></>;
+}
+
+export default App;
